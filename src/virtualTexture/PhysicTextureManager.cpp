8e3bf49341800a4d4ea6c23e1f6654a02c044893
pengchaozheng <45247829+pengchaozheng@users.noreply.github.com>
2020-01-03 08:00:20 +0800

    上传最新

--- src/virtualTexture/PhysicTextureManager.cpp
+++ src/virtualTexture/PhysicTextureManager.cpp
@@ -1,10 +1,50 @@
 #include "PhysicTextureManager.h"
 PhysicTextureManager::PhysicTextureManager(GLsizei k, GLsizei tileSize, GLsizei tileCount,GLenum tileFormat = GL_RGBA, GLsizei maxMipmapLevel = 11, GLuint priorMipLevel = 3) {
-	GLuint* texIds = new GLuint[tileSize+1];
+	GLuint* texIds = new GLuint[2];
 	if (NULL != texIds) {
-		glGenTextures(tileCount + 2, texIds);
-		texId = texIds[0];
+		glGenTextures(2, texIds);
+		texArrayId = texIds[0];
+		texId = texIds[1];
 	}
+	//初始化整张贴图和数组贴图
+	GLenum errorID = GL_NO_ERROR;
+	glBindTexture(GL_TEXTURE_2D_ARRAY, texArrayId);
+	glTextureStorage3D(GL_TEXTURE_2D_ARRAY, maxMipmapLevel, tileFormat, tileSize, tileSize, tileCount);
+	errorID = glGetError();
+	if (GL_NO_ERROR == errorID) {
+		fprintf(stderr, "Init Physic ArrayTexture Success!\n");
+	}
+	else {
+		fprintf(stderr, "init Physic ArrayTexture Failed! Error id:%d\n", errorID);
+	}
+
+	glTextureView(texId
+		, GL_TEXTURE_2D
+		, texArrayId
+		, tileFormat
+		, 0, maxMipmapLevel
+		, 0, tileCount);
+	delete[] texIds;
+
+
+	//初始化分块
+	texIds = new GLuint[tileCount];
+	if (NULL == texIds) {
+		fprintf(stderr,"Init Tile Id Failed\n");
+	}
+	glGenTextures(tileSize,texIds);
+	//创建物理贴图的整张贴图试图
+
+	errorID = glGetError();
+	if (GL_NO_ERROR == errorID) {
+		fprintf(stderr, "Init Physic textureView Success!");
+	}
+	else {
+		fprintf(stderr, "init Physic textureView Failed! Error id:%d\n", errorID);
+	}
+
+
+
 	GLuint width = (tileCount + 1) / 2;
 	tileRows = 2;
 	tileColumns = width;
@@ -22,6 +62,15 @@ PhysicTextureManager::PhysicTextureManager(GLsizei k, GLsizei tileSize, GLsizei 
 		data->mipmapMask = 0;
 		data->interFormat = tileFormat;
 		tileInfoMap[i] = data;
+
+		//每个分块提供一个问题贴图试图
+		glTextureView(texArrayId
+			,GL_TEXTURE_2D
+			,texArrayId
+			,tileFormat
+			,0,11,
+			i,1
+		);
 	}
 }
 void PhysicTextureManager::initPhysicTexture(
@@ -29,30 +78,7 @@ void PhysicTextureManager::initPhysicTexture(
 	,GLsizei tileRows,GLsizei tileColumns
 	,GLsizei tileCount,GLsizei tileSize
 	,GLuint mipMapLevers, GLenum texFormat) {
-	GLenum errorID = GL_NO_ERROR;
-	glBindTexture(GL_TEXTURE_2D_ARRAY, arrayId);
-	glTextureStorage3D(GL_TEXTURE_2D_ARRAY,mipMapLevers,texFormat,tileSize,tileSize,tileCount);
-	errorID = glGetError();
-	if (GL_NO_ERROR == errorID) {
-		fprintf(stderr,"Init Physic ArrayTexture Success!\n");
-	}
-	else {
-		fprintf(stderr,"init Physic ArrayTexture Failed! Error id:%d\n", errorID);
-	}
-	//创建物理贴图的整张贴图试图
-	glTextureView(texId
-	,GL_TEXTURE_2D
-	,texArrayId
-	,texFormat
-	,0,mipMapLevers
-	,0,tileCount);
-	errorID = glGetError();
-	if (GL_NO_ERROR == errorID ) {
-		fprintf(stderr,"Init Physic textureView Success!");
-	}
-	else {
-		fprintf(stderr,"init Physic textureView Failed! Error id:%d\n", errorID);
-	}
+
 }
 
 
@@ -67,7 +93,7 @@ void PhysicTextureManager::init(GLuint arrayTexId,PhysicTileInfo* info) {
 		,GL_TEXTURE_2D
 	,arrayTexId,info->interFormat
 	,0,11
-	,0,100);
+	,info->id,1);
 
 }
 
